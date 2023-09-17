0f1be1fd1bb196d85ddffd3ac7da6aef66dc14c0
Juliano Alfredo <juliano.alfredo86@gmail.com>
2023-09-17 19:41:05 -0300

    Solved BUG, req undefined

--- main.js
+++ main.js
@@ -3,7 +3,11 @@ const multer = require('multer');
 const sqlite3 = require('sqlite3').verbose();
 const app = express();
 const fs = require('fs');
+const cors = require('cors')
+const { resolveObjectURL } = require('buffer');
 const port = 3000;
+
+app.use(express.json()); // For JSON data
 
 const db = new sqlite3.Database('jornalismo-cidadao.db');
 
@@ -17,6 +21,13 @@ db.serialize(() => {
         arquivo TEXT
       )
     `);
+    db.run(`CREATE TABLE IF NOT EXISTS user(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      username TEXT,
+      password TEXT,
+      bio TEXT
+      )
+    `)
   });
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -31,6 +42,7 @@ const storage = multer.diskStorage({
   },
 });
 const upload = multer({ storage });
+
 app.post('/upload', upload.single('file'), (req, res) => {
 
 
@@ -68,8 +80,46 @@ app.get("/getNoticias", (req, res) => {
       console.log(rows);
       res.json({ noticias: rows });
     });
-  });
+});
+
+app.post('/createUser', upload.any(), async (req, res) => {
+  try {
+      const { username, password, bio } = req.body; // Extrair username, password e bio do corpo da requisição
+
+      const sql = `
+          INSERT INTO user (username, password, bio)
+          VALUES (?, ?, ?)
+      `;
+
+      db.run(sql, [username, password, bio], (err) => {
+          if (err) {
+              console.error(err.message);
+              res.status(500).json({ error: "Algo deu errado" });
+              return;
+          }
+          res.json({ message: "Usuário cadastrado com sucesso: " + username });
+      });
+  } catch (error) {
+      console.error(error.message);
+      res.status(400).json({ error: "Requisição inválida" });
+  }
+});
+app.get('/getUser/:usuario', (req, res) =>{
+  const sql = `
+      SELECT * FROM user WHERE username="${req.params.usuario}"
+    `;
+    db.all(sql, [], (err, rows) => {
+      if (err) {
+        console.log(err);
+        res.status(500).json({ error: 'Erro ao buscar usuarios.' });
+        return;
+      }
   
+      console.log(rows);
+      res.json({ usuarios: rows });
+    });
+})
+
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
